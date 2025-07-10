ba6e722364d4359f3fc54c113b881d559a48cbc2
priana13 <herian.sap@gmail.com>
2025-07-11 06:29:26 +0700

    get params

--- app/detail.tsx
+++ app/detail.tsx
@@ -1,9 +1,21 @@
+import { useLocalSearchParams } from 'expo-router';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function DetailsScreen() {
+
+  const params = useLocalSearchParams();
+
+  useEffect(() => {
+
+    console.log(params)
+
+  });
+
+
   return (
     <View style={styles.container}>
-      <Text>Ini adalah halaman detail</Text>
+      <Text>Ini adalah halaman detail : { JSON.stringify(params) }</Text>
     </View>
   );
 }
